08f2233affc432c23d38e1124b4dfd161aae68f8
Ansen Garvin <agarvin@newrelic.com>
2025-12-15 16:56:03 -0800

    chore: strip preexisting headers

--- exporter/nopexporter/nop_exporter.go
+++ exporter/nopexporter/nop_exporter.go
@@ -1,6 +1,3 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
 package nopexporter // import "github.com/newrelic/nrdot-collector-components/exporter/nopexporter"
 
 import (
--- receiver/nopreceiver/nop_receiver.go
+++ receiver/nopreceiver/nop_receiver.go
@@ -1,6 +1,3 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
 package nopreceiver // import "github.com/newrelic/nrdot-collector-components/receiver/nopreceiver"
 
 import (
