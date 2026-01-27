05aa575ef1970378e211af9781a1b52de139d702
Abhinav Tiwari <atiwari@newrelic.com>
2026-01-27 22:59:47 +0530

    refactor: Remove license and add doc.go

---
+++ processor/adaptivetelemetryprocessor/doc.go
@@ -0,0 +1,7 @@
+// Copyright New Relic, Inc. All rights reserved.
+// New Relic Software License
+
+//go:generate mdatagen metadata.yaml
+
+// Package adaptivetelemetryprocessor implements a processor for adaptive telemetry.
+package adaptivetelemetryprocessor // import "github.com/newrelic/nrdot-collector-components/processor/adaptivetelemetryprocessor"
