9383c7d0b3c27432147160db93be9149e3ff8884
Mailo Arsac <marsac@newrelic.com>
2026-04-06 11:48:31 -0700

    [chore] test codecov path filtering on PRs with code changes

--- processor/adaptivetelemetryprocessor/doc.go
+++ processor/adaptivetelemetryprocessor/doc.go
@@ -4,4 +4,5 @@
 //go:generate mdatagen metadata.yaml
 
 // Package adaptivetelemetryprocessor implements a processor for adaptive telemetry.
+// This comment is a test to validate codecov path filtering on PRs - safe to delete.
 package adaptivetelemetryprocessor // import "github.com/newrelic/nrdot-collector-components/processor/adaptivetelemetryprocessor"
