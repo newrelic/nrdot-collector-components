57eab3005d383c3fe63deed25709459ba7b48725
spathlavath <spathlavath@newrelic.com>
2026-05-04 15:53:24 +0530

    1. client_test.go — corrected to // Copyright New Relic, Inc. All rights reserved. (new file header)   2. THIRD_PARTY_NOTICES.md — removed testcontainers-go entry

--- receiver/mysqlreceiver/client_test.go
+++ receiver/mysqlreceiver/client_test.go
@@ -1,9 +1,4 @@
-// Copyright The OpenTelemetry Authors
-// Modifications copyright New Relic, Inc.
-//
-// Modifications can be found at the following URL:
-// https://github.com/newrelic/nrdot-collector-components/commits/main/client_test.go?since=2025-11-26
-//
+// Copyright New Relic, Inc. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 package mysqlreceiver
