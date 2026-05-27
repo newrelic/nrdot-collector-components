a094359299a2b3a4e6b1b85dcc8f6da580aaaa2d
Mailo Arsac <marsac@newrelic.com>
2026-05-27 15:52:33 -0700

    chore: migrate to canonical go tool directive approach, remove install-tools

--- processor/adaptivetelemetryprocessor/doc.go
+++ processor/adaptivetelemetryprocessor/doc.go
@@ -1,7 +1,7 @@
 // Copyright New Relic, Inc. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate mdatagen metadata.yaml
+//go:generate make mdatagen
 
 // Package adaptivetelemetryprocessor implements a processor for adaptive telemetry.
 package adaptivetelemetryprocessor // import "github.com/newrelic/nrdot-collector-components/processor/adaptivetelemetryprocessor"
--- receiver/nopreceiver/doc.go
+++ receiver/nopreceiver/doc.go
@@ -1,7 +1,7 @@
 // Copyright New Relic, Inc. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate mdatagen metadata.yaml
+//go:generate make mdatagen
 
 // Package nopreceiver serves as a placeholder receiver.
 package nopreceiver // import "github.com/newrelic/nrdot-collector-components/receiver/nopreceiver"
